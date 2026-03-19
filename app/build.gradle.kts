4f08b963e6c10dd44f3d3f9b5d958f7426f247d8
042avinash <042avinash@gmail.com>
2026-03-19 18:34:44 +0530

    Bump app version to v1.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.2.7"
+        versionCode = 11
+        versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
