484cea95cd141accdd8363df134fb68321a3675e
042avinash <042avinash@gmail.com>
2026-03-19 11:28:39 +0530

    Bump app version to 1.2.5 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.4"
+        versionCode = 8
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
